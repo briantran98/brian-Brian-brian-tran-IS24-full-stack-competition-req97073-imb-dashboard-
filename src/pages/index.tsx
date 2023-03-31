1bcf3d3f572fc0e7217223623b5a157bba21099d
Brian Tran <98brian.tran@gmail.com>
2023-03-31 04:48:04 -0700

    Moved the table to its own component and tried adding a 3rd part table package.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,52 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { useState, useEffect, MouseEventHandler, useRef } from "react";
+import { useState, useEffect, useRef, MutableRefObject, ChangeEvent } from "react";
 import { Product, ProductResponse } from "@/interfaces/index";
 import ProductForm from "@/components/ProductForm";
-import ToolTipButton from "@/components/ToolTipButton";
+import Table from "@/components/Table";
+import ReactTable from "@/components/ReactTable";
 import { MouseEvent } from "react";
 import useClickAwayListener from "@/hooks/useClickAwayListener";
 
+// Font family from google fonts
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  // Initiliazing states
   const [data, setData] = useState<ProductResponse>({response_code: 0});
-  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isAddingProduct, setIsAddingProduct] = useState<boolean>(false);
   const [isEditing, setIsEditing] = useState<boolean>(false);
   const [selectedProduct, setSelectedProduct] = useState<Product>();
-  const addProductRef = useRef(null);
-  const editProductRef = useRef(null);
+  const [numberOfProducts, setNumberOfProducts] = useState<number>(0);
+  // Initiliazing refs to be used on the page as its rendered
+  const addProductRef : MutableRefObject<null> = useRef(null);
+  const editProductRef : MutableRefObject<null> = useRef(null);
+  // const numberOfProducts : MutableRefObject<number | undefined> = useRef<number>();
 
+  // Adding refs to usehook click away listeners
   useClickAwayListener(addProductRef, setIsAddingProduct);
   useClickAwayListener(editProductRef, setIsEditing);
 
+  // API Call to populate page with mock products
   useEffect(() => {
-    setIsLoading(true);
     fetch("/api/products")
       .then(res => res.json())
       .then(products => {
-        setData(products);
-        setIsLoading(false);
+        setData({...products});
+        const product = (products as ProductResponse);
+        if (product.result) {
+          setNumberOfProducts(product.result.length);
+        }
       });
   }, []);
 
-  const buttonHandler = (event : MouseEvent<HTMLButtonElement>, product?: Product) => {
+  // if (data.result && data.result.length) {
+  //   setNumberOfProducts(data.result.length);
+  // }
+
+  // Button handler to display ProdcutForm component
+  const addButtonHandler = (event : MouseEvent<HTMLButtonElement>, product?: Product) => {
     event.preventDefault();
     const buttonText = (event.target as Node).textContent;
     switch(buttonText) {
@@ -44,36 +57,10 @@ export default function Home() {
       setIsEditing(prev => !prev);
       setSelectedProduct(product);
       break;
+    case "Delete":
+      
     }
   };
-
-  let tableData: JSX.Element[] | null = null;
-  let numberOfProducts: number = 0;
-  if (data.result && data.result.length) {
-    numberOfProducts = data.result.length;
-    tableData = data.result.map(product => {
-      return(
-        <tr className={styles["table-body"]} key={ product.productId }>
-          <td>{ product.productId }</td>
-          <td>{ product.productName }</td>
-          <td>{ product.productOwnerName }</td>
-          <td>{ product.developers?.map(developer => {
-            return(
-              <tr key={ product.productId + developer}>
-                <td>{ developer }</td>
-              </tr>
-            );
-          }) }</td>
-          <td>{ product.scrumMasterName }</td>
-          <td>{ new Date(product.startDate).toISOString().substring(0,10) }</td>
-          <td>{ product.methodology }</td>
-          <td>
-            <ToolTipButton onEditHanlder={e => buttonHandler(e, product)}>...</ToolTipButton>
-          </td>
-        </tr>
-      );
-    });
-  }
 
   return (
     <>
@@ -82,30 +69,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <header className={styles["page-header"]}>
+      <main className={`${inter.className} ${styles.main}`}>
+        <header className={`${styles["page-header"]}`}>
           <h1>{numberOfProducts} Products</h1>
-          <button className={styles["add"]} onClick={buttonHandler}>Add Product</button>
+          {/* <button className={styles["search"]}>Search</button> */}
+          <button className={styles["add"]} onClick={addButtonHandler}>Add Product</button>
         </header>
-        {isAddingProduct && <ProductForm ref={addProductRef} setData={setData} isHidden={!isAddingProduct}/>}
-        {isEditing && <ProductForm ref={editProductRef} setData={setData} product={selectedProduct} isHidden={!isEditing} isEditForm={true}></ProductForm>}
-        <table>
-          <thead >
-            <tr>
-              <th className={styles["table-header"]}>Product Number</th>
-              <th className={styles["table-header"]}>Prodcut Name</th>
-              <th className={styles["table-header"]}>Prodcut Owner Name</th>
-              <th className={styles["table-header"]}>Developers</th>
-              <th className={styles["table-header"]}>Scrum Master Name</th>
-              <th className={styles["table-header"]}>Start Date</th>
-              <th className={styles["table-header"]}>Methodology</th>
-              <th className={styles["table-header"]}></th>
-            </tr>
-          </thead>
-          <tbody>
-            {tableData}
-          </tbody>
-        </table>
+        {/* Conditional Renders for the ProductForm component */}
+        {isAddingProduct && <ProductForm ref={addProductRef} setData={setData} isHidden={!isAddingProduct} setIsHidden={setIsAddingProduct}/>}
+        {isEditing && <ProductForm ref={editProductRef} setData={setData} product={selectedProduct} isHidden={!isEditing} isEditForm={true} setIsHidden={setIsEditing}></ProductForm>}
+        <Table data={data} addButtonHandler={addButtonHandler} setNumberOfProducts={setNumberOfProducts} />
+        {/* <ReactTable values={data} addButtonHandler={addButtonHandler}/> */}
       </main>
     </>
   );
