26557e24fec4f256c8bf4e8ff02a3a77040da3d2
Brian Tran <98brian.tran@gmail.com>
2023-03-30 17:15:07 -0700

    Adding add product and edit product functionality and styling components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,26 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { useState, useEffect } from "react";
-import { ProductResponse } from "@/interfaces/index";
+import { useState, useEffect, MouseEventHandler, useRef } from "react";
+import { Product, ProductResponse } from "@/interfaces/index";
 import ProductForm from "@/components/ProductForm";
+import ToolTipButton from "@/components/ToolTipButton";
+import { MouseEvent } from "react";
+import useClickAwayListener from "@/hooks/useClickAwayListener";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [data, setData] = useState<ProductResponse>({});
-  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState<ProductResponse>({response_code: 0});
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isAddingProduct, setIsAddingProduct] = useState<boolean>(false);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [selectedProduct, setSelectedProduct] = useState<Product>();
+  const addProductRef = useRef(null);
+  const editProductRef = useRef(null);
+
+  useClickAwayListener(addProductRef, setIsAddingProduct);
+  useClickAwayListener(editProductRef, setIsEditing);
 
   useEffect(() => {
     setIsLoading(true);
@@ -22,18 +33,31 @@ export default function Home() {
       });
   }, []);
 
+  const buttonHandler = (event : MouseEvent<HTMLButtonElement>, product?: Product) => {
+    event.preventDefault();
+    const buttonText = (event.target as Node).textContent;
+    switch(buttonText) {
+    case "Add Product":
+      setIsAddingProduct(prev => !prev);
+      break;
+    case "Edit":
+      setIsEditing(prev => !prev);
+      setSelectedProduct(product);
+      break;
+    }
+  };
+
   let tableData: JSX.Element[] | null = null;
-  let numberOfProjects: number = 0;
-  if (data.result && data.result.length)
-  {
-    numberOfProjects = data.result.length;
+  let numberOfProducts: number = 0;
+  if (data.result && data.result.length) {
+    numberOfProducts = data.result.length;
     tableData = data.result.map(product => {
       return(
-        <tr key={ product.productId }>
+        <tr className={styles["table-body"]} key={ product.productId }>
           <td>{ product.productId }</td>
           <td>{ product.productName }</td>
           <td>{ product.productOwnerName }</td>
-          <td>{ product.Developers.map(developer => {
+          <td>{ product.developers?.map(developer => {
             return(
               <tr key={ product.productId + developer}>
                 <td>{ developer }</td>
@@ -41,8 +65,11 @@ export default function Home() {
             );
           }) }</td>
           <td>{ product.scrumMasterName }</td>
-          <td>{ product.startDate.toLocaleString() }</td>
+          <td>{ new Date(product.startDate).toISOString().substring(0,10) }</td>
           <td>{ product.methodology }</td>
+          <td>
+            <ToolTipButton onEditHanlder={e => buttonHandler(e, product)}>...</ToolTipButton>
+          </td>
         </tr>
       );
     });
@@ -56,21 +83,23 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <ProductForm setData={setData}/>
-        <header>
-          <h1>{numberOfProjects} Projects</h1>
-          <button>Add Project</button>
+        <header className={styles["page-header"]}>
+          <h1>{numberOfProducts} Products</h1>
+          <button className={styles["add"]} onClick={buttonHandler}>Add Product</button>
         </header>
+        {isAddingProduct && <ProductForm ref={addProductRef} setData={setData} isHidden={!isAddingProduct}/>}
+        {isEditing && <ProductForm ref={editProductRef} setData={setData} product={selectedProduct} isHidden={!isEditing} isEditForm={true}></ProductForm>}
         <table>
-          <thead>
+          <thead >
             <tr>
-              <td>Product Number</td>
-              <th>Prodcut Name</th>
-              <th>Prodcut Owner Name</th>
-              <th>Developers</th>
-              <th>Scrum Master Name</th>
-              <th>Start Date</th>
-              <th>Methodology</th>
+              <th className={styles["table-header"]}>Product Number</th>
+              <th className={styles["table-header"]}>Prodcut Name</th>
+              <th className={styles["table-header"]}>Prodcut Owner Name</th>
+              <th className={styles["table-header"]}>Developers</th>
+              <th className={styles["table-header"]}>Scrum Master Name</th>
+              <th className={styles["table-header"]}>Start Date</th>
+              <th className={styles["table-header"]}>Methodology</th>
+              <th className={styles["table-header"]}></th>
             </tr>
           </thead>
           <tbody>
